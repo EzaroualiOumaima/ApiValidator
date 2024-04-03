5312461ac0762e99cfeb51c4fca54eb11491d826
oumaimaezarouali <oumaima.2001ezarouali@gmail.com>
2024-04-03 11:01:27 +0000

    add test

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -27,7 +27,6 @@ const registerUser = async (req, res) => {
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
